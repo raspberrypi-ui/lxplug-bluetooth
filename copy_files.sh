e6d8c3470875cdda8019a489bee898a97a69bfed
spl <simon@raspberrypi.org>
2015-02-23 17:18:07 +0000

    Gitignore updated.

---
+++ copy_files.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# lxpanel
+sudo cp -p /usr/local/bin/lxpanel* /media/root/usr/local/bin/
+sudo cp -pr /usr/local/include/lxpanel/ /media/root/usr/local/include/
+sudo cp -pr /usr/local/lib/lxpanel/ /media/root/usr/local/lib/
+sudo cp -pr /usr/local/share/lxpanel/ /media/root/usr/local/share/
