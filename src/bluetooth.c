87c1fbadfac1e0c1743070bffd5a85f5179423a2
Simon Long <simon@raspberrypi.com>
2025-01-14 09:23:37 +0000

    Add GNU_SOURCE to meson

--- src/bluetooth.c
+++ src/bluetooth.c
@@ -25,8 +25,6 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ============================================================================*/
 
-#define _GNU_SOURCE
-
 #include <errno.h>
 #include <locale.h>
 #include <stdlib.h>
